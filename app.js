1f439b1f428ec814bf1348831ece6c8e017213ef
reaz uddin mahmood <reaz.u.mahmood@gmail.com>
2018-03-08 15:06:14 +0100

    first installment

--- app.js
+++ app.js
@@ -85,8 +85,8 @@ document.getElementById('current-0').textContent = "0";
 document.getElementById('score-1').textContent = "0";
 document.getElementById('current-1').textContent = "0";
 document.querySelector(".dice").style.display = 'none';
-document.querySelector("#name-0").textContent = "Player 1"
-document.querySelector("#name-1").textContent = "Player 2"
+document.querySelector("#name-0").textContent = "Player 1";
+document.querySelector("#name-1").textContent = "Player 2";
 
 
 document.querySelector(".player-0-panel").classList.remove("active");
